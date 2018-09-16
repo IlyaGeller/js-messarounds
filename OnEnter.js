69104935821539b16ccb7140d4766e64b8474527
Asaf Inbar <asaf@chatway.com>
2018-09-16 15:20:57 +0300

    next

--- OnEnter.js
+++ OnEnter.js
@@ -1,19 +1,50 @@
-document.getElementById('input').addEventListener("keyup",function(ev){
-    console.log(ev.keyCode);
-    if(ev.keyCode == 13){
-        if(ev.currentTarget.value == 123){
-            alert("Correct!");
-        }
-        else{
-            alert("Nope");
-        }
-    }
-});
+const step_count = 5;
+var direction = 1;
+var tar = document.getElementById("target");
+var isClicked = false;
+var keysDown = {left:false,right:false,up:false,down:false};
+var keys = {left:37, right:39, up:38, down:40};
 
-document.getElementById("div_password").setAttribute("style","width:200px");
-// document.getElementById("div_password").setAttribute("position","width:500px");
-// document.addEventListener("keyup",function(ev){
-//     if(ev.keyCode==37){
-//         document.getElementById("square1").setAttribute("left",
-//     }
-// })
+const PLAYER_DIR = {
+    LEFT: 0,
+    RIGHT: 1,
+    UP: 2,
+    DOWN: 3
+};
+
+
+
+function Player() {
+    this.element = null;
+    this.pos = {x:0,y:0,dir:PLAYER_DIR.RIGHT};
+    this.loop = function(){
+        setInterval(()=>{
+            this.element.style.left = this.pos.x +'px';
+            this.element.style.top = this.pos.y + 'px';
+        },10)
+    }
+    this.init = function(element){
+        this.element=element;
+
+        document.addEventListener("keydown",(ev)=>{
+            switch(ev.keyCode){
+                case 37: 
+                    this.pos.x--;
+                    break;
+                case 39:
+                    this.pos.x++;
+                    break;
+                case 38: 
+                    this.pos.y--;
+                    break;
+                case 40: 
+                    this.pos.y++;
+                    break;
+            }
+        })
+    }
+}
+
+const player = new Player();
+player.init(document.getElementById("target"));
+player.loop();
